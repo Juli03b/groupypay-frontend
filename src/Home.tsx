729f36c5fc6645d71ec0dde3ea7b7ee580b2760f
Julio Barrios <julio.junior02@gmail.com>
2022-01-18 15:50:34 -0600

    Change home text

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,22 +2,26 @@ import { Box, Button, Grid, styled, Typography } from "@mui/material";
 import { FC } from "react";
 
 const StyledHeader = styled(Typography)(({theme}) => ({
-    padding: theme.spacing(2.5),
+    padding: theme.spacing(1),
     fontWeight: theme.typography.fontWeightLight
 }));
 
 const Home: FC = () => {
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12} md={12}>
-                <StyledHeader variant="h1">Make your <b>groupypayments</b> easier!</StyledHeader>
+        <Grid       
+            container
+            alignItems={"center"}
+            justifyContent={"space-around"}
+            minHeight={"70vh"}
+            spacing={2}
+        >
+            <Grid item xs={12} xl={6}>
+                <StyledHeader variant="h1">Make your <b style={{fontSize: "70%", display:"block"}}>groupypayments</b> easier!</StyledHeader>
             </Grid>
-            <Grid item xs={12} md={12}>
-                <Box>
-
-                    <StyledHeader variant="h4"><b>Organize and divvy up group payments quickly</b></StyledHeader>
+            <Grid item xs={12} xl={4}>
+            <Box textAlign={"center"}>
+                <StyledHeader variant="h3"><b>Organize and divvy up group payments quickly</b></StyledHeader>
                     <Button color="primary" variant="contained">Get started now</Button>
-
                 </Box>
             </Grid>
         </Grid>
