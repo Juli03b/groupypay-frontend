3fe26c5a895512da2cc9f57c782af0b2452206ba
Julio Barrios <julio.junior02@gmail.com>
2022-01-19 18:37:58 -0600

    Add navbar

--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,5 +1,32 @@
-const Nav = () => {
-    return <h2>Nav here</h2>;
+import { FC } from "react";
+import { Container, Navbar, NavDropdown, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Navigation: FC = () => {
+    const isAuth: boolean = false;
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Container>
+            <Navbar.Brand href="/">groupypay</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                {!isAuth && <Nav.Link href="/">Home</Nav.Link>}
+                {!isAuth && <Nav.Link href="/sign-up">Sign Up</Nav.Link>}
+                {!isAuth && <Nav.Link href="/sign-in">Sign In</Nav.Link>}
+                {isAuth && <Nav.Link href="/dashboard">Dashboard</Nav.Link>}
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown> */}
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>    
+    )
 }
 
-export default Nav;
+export default Navigation;
