683fd54282fe3977634fbce8cad9f8a491368aa8
Julio Barrios <julio.junior02@gmail.com>
2022-01-20 16:18:03 -0600

    remove some properties and add signOut funtion

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -3,17 +3,19 @@ import { UserTokenProps } from "./interfaces";
 
 interface AppContextProps {
     user: UserTokenProps | undefined;
-    userToken: UserTokenProps | undefined;
-    token?: string | undefined;
+    // userToken: UserTokenProps | undefined;
+    // token?: string | undefined;
     signIn: Function;
     signUp: Function;
+    signOut: Function;
     patchUser: Function;
 }
 
 export default createContext<AppContextProps>({
     user: undefined,
-    userToken: undefined,
+    // userToken: undefined,
     signIn: Function,
     signUp: Function,
+    signOut: Function,
     patchUser: Function
 });
