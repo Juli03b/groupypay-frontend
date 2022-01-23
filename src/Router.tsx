473f63e057b4d725c2d4b655a3aeaf2500b74af6
Julio Barrios <julio.junior02@gmail.com>
2022-01-22 19:00:09 -0600

    Add routes to router

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { FC, useContext } from "react";
 import { Navigate, Route, Routes } from "react-router";
 import AppContext from "./AppContext";
 import Dashboard from "./Dashboard";
+import Group from "./Group";
 import Home from "./Home";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
@@ -10,10 +11,10 @@ const Router: FC = () => {
     const { user } = useContext(AppContext);
     return (
     <Routes>
-        {/* {!isAuth && <Route path="/sign-up" element={<SignUp/>} />} // Users must be signed out */}
         {!user && <Route path="/sign-up" element={<SignUp/>} />} // Users must be signed out
         {!user && <Route path="/sign-in" element={<SignIn/>} />} // Users must be signed out
         {user && <Route path="/dashboard" element={<Dashboard/>} />} // Users must be signed in
+        {user && <Route path="/users/:email/groups/:groupId" element={<Group />} />}        
         {!user && <Route path="/" element={<Home/>} />} // Signed in are redirected to /dashboard
         {user && <Route path="/" element={<Navigate replace to={"/dashboard"}/>} />} // Signed in are redirected to /dashboard
         
