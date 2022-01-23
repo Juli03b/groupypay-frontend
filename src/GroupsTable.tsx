e68476a065a2918ee29df781b2bbc2c629153d7e
Julio Barrios <julio.junior02@gmail.com>
2022-01-22 18:59:08 -0600

    Change GroupsTable

--- src/GroupsTable.tsx
+++ src/GroupsTable.tsx
@@ -7,6 +7,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Link } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import AppContext from './AppContext';
+import GroupypayApi from './GroupypayApi';
+import { GroupProps } from './interfaces';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -37,38 +41,46 @@ function createData(
   return { name, members, description, id };
 }
 
-const rows = [
-  createData('Frozen yoghurt', 15, "hello", 2),
-  createData('Ice cream sandwich', 237, "helllllloooooo", 1),
-];
 
-const GroupsTable = () => {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 700 }} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Name</StyledTableCell>
-            <StyledTableCell align="right">Members</StyledTableCell>
-            <StyledTableCell align="right">Description</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-              <StyledTableRow key={row.name}>
-                    <Link to={`/groups/${row.id}`}>
-                <StyledTableCell component="th" scope="row">
-                    {row.name}
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.members}</StyledTableCell>
-                <StyledTableCell align="right">{row.description}</StyledTableCell>
-            </Link>
-                </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+const GroupsTable = ({groups, email}: {groups: GroupProps[], email: string}) => {
+    const [rows, setRows] = useState<any[]>([]);
+
+    useEffect(() => {
+        const rows = []
+        for (let group of groups) {
+            rows.push(createData(group.name, group.members || 0, group.description, group.id))
+        }
+        setRows(rows)
+    }, [groups])
+
+    return (
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 500 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Name</StyledTableCell>
+              <StyledTableCell align="right">Members</StyledTableCell>
+              <StyledTableCell align="right">Description</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { rows ? rows.map((row) => (
+                <StyledTableRow key={row.id}>
+                  <StyledTableCell component="th" scope="row">
+                      <Link to={`/users/${email}/groups/${row.id}`}>
+                          {row.name}
+                      </Link>
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.members}</StyledTableCell>
+                  <StyledTableCell align="right">{row.description}</StyledTableCell>
+                  </StyledTableRow>
+            ))
+              :
+              <h1>NONE</h1>}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
 }
 
 export default GroupsTable;
