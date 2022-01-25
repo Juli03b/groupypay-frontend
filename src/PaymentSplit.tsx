dc880cff6acf54d0ff1254697431869019f426cf
Julio Barrios <julio.junior02@gmail.com>
2022-01-25 15:05:39 -0600

    Add component to facilitate adding payments

---
+++ src/PaymentSplit.tsx
@@ -0,0 +1,67 @@
+import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+import { MemberProps } from "./interfaces";
+
+function createData(
+    id: number,
+    name: string,
+    email: string,
+    phone_number: string
+) {
+  return { id, name, email, phone_number }
+}
+
+const PaymentSplit = ({members, formValues, disabled, onChange}:{members: MemberProps[], formValues: any, disabled: boolean, onChange: Function}) => {
+    const [rows, setRows] = useState<any[]>([]);
+
+    
+    useEffect(() => {
+        const rows = []
+        console.log("members:", members)
+        for (let member of members) {
+          rows.push(createData(member.id, member.name, member.email, member.phone_number))
+        }
+
+        setRows(rows);
+
+    }, [members]);
+
+    return (
+        <TableContainer component={Paper} sx={disabled ? {pointerEvents: "none", opacity: 0.7} : {}}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Amount</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell>
+                  <TextField
+                    id="outlined-number"
+                    label=""
+                    type="number"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    value={formValues[row.id]}
+                    onChange={(values) => onChange(values, row.id)}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+    );
+}
+
+export default PaymentSplit;
