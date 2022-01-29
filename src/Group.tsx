18d7db8fd8d32ed148c20040c966849b274d18da
Julio Barrios <julio.junior02@gmail.com>
2022-01-29 14:29:32 -0600

    Fix member count

--- src/Group.tsx
+++ src/Group.tsx
@@ -9,34 +9,49 @@ import GroupypayApi from "./GroupypayApi";
 import { GroupPaymentProps, GroupProps, MemberPaymentProps, MemberProps } from "./interfaces";
 import MembersTable from "./MembersTable";
 import PaymentsTable from "./PaymentsTable";
+import { useAlert } from "./hooks";
+import PaymentPopup from "./PaymentPopup";
 
 const Group = () => {
+    const alert = useAlert();
     const { groupId } = useParams();
     const [group, setGroup] = useState<GroupProps | undefined>();
-    const [members, setMembers] = useState<MemberProps[]>([]);
+    const [members, setMembers] = useState<MemberProps>({});
     const [payments, setPayments] = useState<GroupPaymentProps[]>([]);
     const [addMember, setAddMember] = useState<boolean>(false);
     const [addPayment, setAddPayment] = useState<boolean>(false);
+    const [paymentOpen, setPaymentOpen] = useState<GroupPaymentProps | undefined>(undefined);
 
     const handleMemberSubmit = async (memberFromForm: MemberProps) => {
         if (!groupId) return;
         const {member} = await GroupypayApi.addMember(groupId, memberFromForm);
-        setMembers(members => [...members, member])
+        console.log("MEMBER",member)
+        console.log("MEMBERS B4", members)
+        setMembers((members) => ({...members, [member.id]: member}));
     }
 
-    const handlePaymentSubmit = async (paymentFromForm: GroupPaymentProps, memberPayments: MemberPaymentProps[]) => {
+    const handlePaymentSubmit = async (paymentFromForm: GroupPaymentProps, memberPayments: MemberPaymentProps[], memberPaid: number) => {
         if (!groupId) return;
-        const groupPayment = await GroupypayApi.addPayment(groupId, paymentFromForm, memberPayments);
-        setPayments(payments => [...payments, groupPayment])
+        try {
+            const payment = await GroupypayApi.addPayment(groupId, paymentFromForm, memberPayments, memberPaid);
+            setPayments(payments => [...payments, payment])
+            
+          } catch (error: any) {
+            alert(error, "error")
+          }
     }
 
     useEffect(() => {
         if (!groupId) return;
         const groupRes = async () => {
             const group: GroupProps = await GroupypayApi.getGroup(groupId);
+            console.log(group)
             setMembers(group.members)
             setPayments(group.payments)
             setGroup(group);
+            console.log("MEMBERS", members)
+
+            console.log("payments", payments)
         }
         groupRes()
 
@@ -44,26 +59,54 @@ const Group = () => {
 
     return (
             group ? (
-                <Container sx={{marginTop: "15vh"}}>
+                <Container sx={{marginY: "10vh"}}>
                     <Box sx={{marginBottom: "10vh"}}>
                         <Typography variant="h1">{group?.name}</Typography>
-                        <Typography variant="caption">{group?.description}</Typography>    
+                        <Typography variant="caption" sx={{color: (theme) => theme.palette.grey[700] , fontSize: "1.5em"}}>
+                            {group.description}
+                        </Typography> 
                     </Box>
+                    
+                    <Box>
+                        <Typography variant="h6" textAlign="left">
+                            The group has spent a total of 
+                        </Typography>
+                        <Typography variant="overline" sx={{fontSize: "1em"}}>
+                                <b>$</b>{payments.reduce((prevPayment: number, currentPayment: GroupPaymentProps) => {
+                                return prevPayment + parseInt(currentPayment.total_amount);
+                            }, 0)}
+                        </Typography>
+                    </Box>
+
+                    <hr />
+
+                    {/* Payments */}
                     {
-                        members && (
-                            <>
-                                <h1><b>Add expense</b></h1><IconButton aria-label="add-member" onClick={() => setAddPayment(true)} ><Add/></IconButton>
+                        (members && !!(Object.keys(members).length)) && (
+                            <Box sx={{marginY: "2.5vh"}}>
+                                <Typography variant="h3" sx={{display: "inline"}} gutterBottom>Add expense</Typography>
+                                <IconButton aria-label="add-member" onClick={() => setAddPayment(true)} ><Add sx={{marginBottom: "16px"}}/></IconButton>
                                 {addPayment && <AddPayment handleClose={() => setAddPayment(false)} open={addPayment} addPayment={handlePaymentSubmit} members={members} />}
-                            </>)
+                                {paymentOpen && <PaymentPopup handleClose={() => setPaymentOpen(undefined)} payment={paymentOpen} members={members} />}
+
+                                <PaymentsTable payments={payments} onClick={(groupPayment: GroupPaymentProps) => {
+                                    setPaymentOpen(groupPayment)
+                                }
+                                }/>
+                            </Box>)
                     }
-                    <PaymentsTable payments={payments} groupId={groupId} />
-                    <h1><b>Members</b></h1><IconButton aria-label="add-member" onClick={() => setAddMember(true)} ><Add/></IconButton>
-                    {addMember && <AddMember handleClose={() => setAddMember(false)} open={addMember} addMember={handleMemberSubmit} />}
-                    <MembersTable members={members} />
+
+                    {/* Members */}
+                    <Box>
+                        <Typography variant="h3" sx={{display: "inline"}} gutterBottom>Members</Typography>
+                        <IconButton aria-label="add-member" onClick={() => setAddMember(true)}><Add sx={{marginBottom: "16px"}}/></IconButton>
+                        {addMember && <AddMember handleClose={() => setAddMember(false)} open={addMember} addMember={handleMemberSubmit} />}
+                        {(members && !!Object.keys(members).length) && <MembersTable members={members} />  }
+                    </Box>
                 </Container>
             ) :
             (
-                <p>Hellow</p>
+                <Typography variant="h1" sx={{color: "gray", fontWeight: 600, marginTop: "25vh"}} textAlign={"center"}>404</Typography>
             )
     )
 }
