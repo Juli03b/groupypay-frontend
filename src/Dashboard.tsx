fb83c02656b641d2e58c419959ad609b1372799a
Julio Barrios <julio.junior02@gmail.com>
2022-02-23 14:08:25 -0600

    Add importing from material-ui and implement new group cards

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,17 +1,24 @@
 import { Add } from "@mui/icons-material";
-import { Box, Container, IconButton, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Card, CardActionArea, Container, Divider, IconButton, Paper, Tab, Tabs, Typography } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React, { FC, useContext, useEffect, useState } from "react";
 import AppContext from "./AppContext";
+import GroupCard from "./GroupCard";
 import GroupsTable from "./GroupsTable";
 import GroupypayApi from "./GroupypayApi";
+import { useAlert } from "./hooks";
 import { GroupProps } from "./interfaces";
+import Loading from "./Loading";
 import MakeGroup from "./MakeGroup";
+import MemberPaymentCard from "./MemberPaymentCard";
+import MemberPaymentCardList from "./MemberPaymentCardList";
 
 const Dashboard: FC = () => {
-    const { user } = useContext(AppContext);
+    const { user, signOut } = useContext(AppContext);
     const [makeGroup, setMakeGroup] = useState<boolean>(false);
+    const [loading, setLoading] = useState<boolean>(false);
     const [groups, setGroups] = useState<GroupProps[]>([]);
+    const alert = useAlert();
     const closeMakeGroup = () => {
         setMakeGroup(false);
     }
@@ -19,31 +26,64 @@ const Dashboard: FC = () => {
     useEffect(() => {
         if (user?.email) {
             const getGroups = async () => {
-                const groupsRes: GroupProps[] = await GroupypayApi.getUserGroups(user.email);
-                setGroups(groupsRes);
+                setLoading(true)
+                try {
+                    const groupsRes: GroupProps[] = await GroupypayApi.getUserGroups(user.email);
+                    setGroups(groupsRes);
+                        
+                } catch (error: any) {
+                    signOut();
+                    alert(error, "error")
+                }
+                setLoading(false)
             }
             getGroups();
         }
     }, [user])
-
+    
+    if (loading) return <Loading />;
     return (
         user ? (
             <Container sx={{marginY: "10vh"}}>
-                <Box>
-                    <Typography variant="h3" sx={{display: "inline", fontSize: "3em"}} gutterBottom>Groups</Typography>
-                    <IconButton aria-label="add-group"  onClick={() => setMakeGroup(true)} sx={{display: "inline"}}><Add sx={{marginBottom: "16px"}}/></IconButton>
+                <Box mb="2.5vh">
                     {makeGroup && <MakeGroup handleClose={closeMakeGroup} open={makeGroup} addGroup={(group: GroupProps) => setGroups(groups => [...groups, group])} />}
-                    <GroupsTable groups={groups} email={user?.email} />
+                    <Typography variant="h6" sx={{display: "inline", fontSize: "2em"}} gutterBottom>Groups</Typography>
+                    {/* <Paper style={{maxHeight: 500, overflow: "auto"}}> */}
+                        <Box sx={{marginY: "1vh"}}>
+                            {
+                                groups.map((group: GroupProps) => {
+                                    return <GroupCard email={user.email} group={group} key={group.id} />
+                                }).concat([(
+                                    <Card sx={{height: 255, minWidth: 150, display: "inline-block"}}>
+                                        <CardActionArea sx={{height: "100%"}} onClick={() => setMakeGroup(true)}>
+                                            <Add sx={{
+                                                position: "absolute",
+                                                left: "55px",
+                                                bottom: "100px"
+                                            }} />
+
+                                        </CardActionArea>
+                                    </Card>
+                                )])
+                            }
+                        </Box>
+                    {/* </Paper> */}
                 </Box>
+                {   
+                    (user.owed_payments && user.owed_payments.length >= 1) && (
+                        <Box>
+                            <Typography variant="h6" sx={{display: "inline", fontSize: "2em"}} gutterBottom>Payments you owe</Typography>
+                            <Box marginY={"1vh"}>
+                                <MemberPaymentCardList email={user.email} memberPaymentsProp={user.owed_payments} />
+                            </Box>
+                        </Box>
+                    )
+                }
             </Container>
         ) :
         <p>Hello</p>
-    )
+    );
 
 }
 
 export default Dashboard;
-
-function createData(name: string, arg1: any, description: string, id: number): any {
-    throw new Error("Function not implemented.");
-}
